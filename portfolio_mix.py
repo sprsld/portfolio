3a84fa0550bd7d8d910a391fc91318756a92be33
alext251 <alexorr.cse@gmail.com>
2022-03-17 23:09:56 -0700

    change tabs to spaces

--- portfolio_mix.py
+++ portfolio_mix.py
@@ -69,71 +69,71 @@ def calc_portfolio_expected_return(percent_a, prices_a, percent_b, prices_b):
     return percent_a * er_a + percent_b * er_b
 
 def get_args():
-	parser = argparse.ArgumentParser(description='Fund Tickers')
-	parser.add_argument('-tickers', metavar='VTSAX VDADX', type=str, nargs=2, required=True)
-	parser.add_argument('-price_history', type=int, default=6)
+    parser = argparse.ArgumentParser(description='Fund Tickers')
+    parser.add_argument('-tickers', metavar='VTSAX VDADX', type=str, nargs=2, required=True)
+    parser.add_argument('-price_history', type=int, default=6)
 
-	return parser.parse_args()
+    return parser.parse_args()
 
 def get_close_prices(client, ticker):
-	historical_prices = client.get_ticker_price(ticker,
-	                                            fmt='json',
-	                                            startDate=START_DATE,
-	                                            endDate=TODAY,
-	                                            frequency='daily')
-	return [price['close'] for price in historical_prices]
+    historical_prices = client.get_ticker_price(ticker,
+                                                fmt='json',
+                                                startDate=START_DATE,
+                                                endDate=TODAY,
+                                                frequency='daily')
+    return [price['close'] for price in historical_prices]
 
 
 def main():
-	global TODAY
-	global START_DATE
+    global TODAY
+    global START_DATE
 
-	args = get_args()
-	client = TiingoClient()
+    args = get_args()
+    client = TiingoClient()
 
-	TODAY = date.today()
-	START_DATE = TODAY + relativedelta(months=-args.price_history)
+    TODAY = date.today()
+    START_DATE = TODAY + relativedelta(months=-args.price_history)
 
-	ticker_a = args.tickers[0]
-	ticker_b = args.tickers[-1]
+    ticker_a = args.tickers[0]
+    ticker_b = args.tickers[-1]
 
-	prices_a = get_close_prices(client, ticker_a)
-	prices_b = get_close_prices(client, ticker_b)
+    prices_a = get_close_prices(client, ticker_a)
+    prices_b = get_close_prices(client, ticker_b)
 
-	if len(prices_a) != len(prices_b):
-		print("WARNING! You have gone past the total lifetime of at least one of your assets."
-			  " This may affect model accuracy.")
+    if len(prices_a) != len(prices_b):
+        print("WARNING! You have gone past the total lifetime of at least one of your assets."
+              " This may affect model accuracy.")
 
-	correlation_ab = calc_correlation(prices_a, prices_b)
+    correlation_ab = calc_correlation(prices_a, prices_b)
 
-	expected_returns = []
-	standard_deviations = []
-	for percent in range (0,101):
-	    variance = calc_portfolio_variance(percent, prices_a, (100-percent), prices_b, correlation_ab)
-	    stdev = math.sqrt(variance)
+    expected_returns = []
+    standard_deviations = []
+    for percent in range (0,101):
+        variance = calc_portfolio_variance(percent, prices_a, (100-percent), prices_b, correlation_ab)
+        stdev = math.sqrt(variance)
 
-	    expected_ret = calc_portfolio_expected_return(percent, prices_a, (100-percent), prices_b)
+        expected_ret = calc_portfolio_expected_return(percent, prices_a, (100-percent), prices_b)
 
-	    standard_deviations.append(stdev)
-	    expected_returns.append(expected_ret)
+        standard_deviations.append(stdev)
+        expected_returns.append(expected_ret)
 
-	plt.title(f"Return vs. Risk\n{int(args.price_history/12)} year model")
-	plt.plot(standard_deviations, expected_returns, 'b')
-	plt.ylabel('Expected Return (%)')
-	plt.xlabel('Standard Deviation (%)')
+    plt.title(f"Return vs. Risk\n{int(args.price_history/12)} year model")
+    plt.plot(standard_deviations, expected_returns, 'b')
+    plt.ylabel('Expected Return (%)')
+    plt.xlabel('Standard Deviation (%)')
 
-	# Plot Minimum Variance
-	percent_a, percent_b = calc_minimum_variance_allocation(prices_a, prices_b)
-	min_var_er = calc_portfolio_expected_return(percent_a * 100, prices_a, percent_b * 100, prices_b)
-	min_var_stdev = math.sqrt(calc_portfolio_variance(percent_a * 100, prices_a, percent_b * 100, prices_b, correlation_ab))
+    # Plot Minimum Variance
+    percent_a, percent_b = calc_minimum_variance_allocation(prices_a, prices_b)
+    min_var_er = calc_portfolio_expected_return(percent_a * 100, prices_a, percent_b * 100, prices_b)
+    min_var_stdev = math.sqrt(calc_portfolio_variance(percent_a * 100, prices_a, percent_b * 100, prices_b, correlation_ab))
 
-	percent_a_label = int(math.ceil(percent_a * 100))
-	percent_b_label = int(math.floor(percent_b * 100))
-	min_var_label = "{} {}%, {} {}%".format(ticker_a, percent_a_label, ticker_b, percent_b_label)
-	plt.plot(min_var_stdev, min_var_er, 'ro-')
-	plt.annotate(min_var_label, (min_var_stdev, min_var_er))
+    percent_a_label = int(math.ceil(percent_a * 100))
+    percent_b_label = int(math.floor(percent_b * 100))
+    min_var_label = "{} {}%, {} {}%".format(ticker_a, percent_a_label, ticker_b, percent_b_label)
+    plt.plot(min_var_stdev, min_var_er, 'ro-')
+    plt.annotate(min_var_label, (min_var_stdev, min_var_er))
 
-	plt.show()
+    plt.show()
 
 if __name__ == "__main__":
-	main()
+    main()
